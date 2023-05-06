d0bd4a711cabc7da23e76ed2a914601d7fb14365
Vanderson Nascimento <vando.nsc@gmail.com>
2023-05-06 17:58:00 -0300

    Mudado a estrutura de repetição de while para for

--- main.js
+++ main.js
@@ -3,19 +3,18 @@ function tocaSom(idElementoAudio){
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador = 0;
 
-//enquanto 
-while( contador < listaDeTeclas.length){//.length =atributo usado para ver o tamanho da constante
+//para
+for ( let contador = 0; contador < listaDeTeclas.length; contador++){//.length =atributo usado para ver o tamanho da constante
+
     const tecla = listaDeTeclas[contador];
     const instrumento = listaDeTeclas[contador].classList[1];
     //template string
     const idAudio  = `#som_${instrumento}`
-    console.log(idAudio);
 
     listaDeTeclas[contador].onclick = function(){
         tocaSom(idAudio)
     };
-    contador = contador + 1;
-    console.log(contador);
+    //console.log(contador);
+
 }
