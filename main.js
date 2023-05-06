3040fa282bb7bbe57f7a690d636f9ae5e77da209
Vanderson Nascimento <vando.nsc@gmail.com>
2023-05-06 17:30:24 -0300

    Implementado o som em todas as teclas - Criada 3  novas referências constantes - Implementado while como sistema de repetição - Feito uma função anônima para receber uma constante - Usado console.log para testes

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
+
+//enquanto 
+while( contador < listaDeTeclas.length){//.length =atributo usado para ver o tamanho da constante
+    const tecla = listaDeTeclas[contador];
+    const instrumento = listaDeTeclas[contador].classList[1];
+    //template string
+    const idAudio  = `#som_${instrumento}`
+    console.log(idAudio);
+
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom(idAudio)
+    };
+    contador = contador + 1;
+    console.log(contador);
+}
